78e516b5b97dc63545d9c730dac2952f85eaf17d
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-08-09 14:38:41 +0200

    insert into

---
+++ exercice_passager_personne/insert_rows.sql
@@ -0,0 +1,17 @@
+INSERT INTO `people`.`passager`
+(`nom`, `prenom`, `numero_vol`, `nombre_bagage`, `numero_client`)
+VALUES 
+('Einstein', 'Albert', 45622, 2, 154565),
+('Lavoisier', 'Antoine', 45644, 4, 235002),
+('Raimbault', 'Arthur', 12696, 2, 544552),
+('Poincarre', 'Henri', 45644, 3, 781201),
+('Lavoisier', 'Antoine', 45644, 1, 785154),
+('Einstein', 'Albert', 75906, 0, 858547);
+INSERT INTO `people`.`personne`
+(`nom`, `prenom`, `age`, `civilite`)
+VALUES
+('Einstein', 'Albert', 45, 'marie'),
+('Lavoisier', 'Antoine', 41, 'marie'),
+('Planck', 'Max', 52, 'veuf'),
+('Poincarre', 'Henri', 45, 'marie'),
+('Raimbault', 'Arthur', 25, 'celibataire');
